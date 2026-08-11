7512cd9814334bf4f9ef93dc1f37d6807a402a8c
Simon Dalmasso <simondalmasso44@gmail.com>
2026-08-11 04:16:07 -0300

    chore(score-002): remove temporary repair script

--- scripts/score002_autofix.py
+++
@@ -1,9 +0,0 @@
-from score002_autofix_pre import *
-try:
-    from score002_autofix_clean import *
-except Exception as exc:
-    print(f"clean transformer stopped; continuing post repair: {exc!r}")
-from score002_autofix_post import *
-from score002_autofix_post2 import *
-from score002_autofix_post3 import *
-from score002_commit_now import *
